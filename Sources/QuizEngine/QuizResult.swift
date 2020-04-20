35f3c5d67bb25985936ca7e95926d071ce2672ab
Jero Sánchez <jsanchez@loycus.com>
2020-04-20 19:39:54 +0200

    Fix access control of QuizResult

--- Sources/QuizEngine/QuizResult.swift
+++ Sources/QuizEngine/QuizResult.swift
@@ -3,4 +3,9 @@ import Foundation
 public struct QuizResult <Question: Hashable, Answer> {
     public let answers: [Question: Answer]
     public let score: Int
+    
+    public init(answers: [Question: Answer], score: Int) {
+        self.answers = answers
+        self.score = score
+    }
 }
