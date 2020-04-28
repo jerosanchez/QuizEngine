5943cbdca3504322d481b29f64da6803b2a46bd4
Jero Sánchez <jsanchez@loycus.com>
2020-04-28 20:18:30 +0200

    Add Question model

---
+++ Sources/QuizEngine/Question.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+public enum Question<T: Hashable>: Hashable {
+    case singleAnswer(T)
+    case multipleAnswer(T)
+    
+    public func hash(into hasher: inout Hasher) {
+       switch self {
+        case .singleAnswer(let value):
+            hasher.combine(value)
+        case .multipleAnswer(let value):
+            hasher.combine(value)
+        }
+    }
+    
+    public static func ==(lhs: Question<T>, rhs: Question<T>) -> Bool {
+        switch (lhs, rhs) {
+        case (.singleAnswer(let a), .singleAnswer(let b)):
+            return a == b
+        case (.multipleAnswer(let a), .multipleAnswer(let b)):
+            return a == b
+        default:
+            return false
+        }
+    }
+}
